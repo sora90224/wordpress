f3dfdd9265e16dffdb8abbf5e419033738fbc7cb
thisgun <thisgun@naver.com>
2016-06-10 13:28:57 +0900

    files delete

--- lib/define.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('SIR_CMM_NAME', 'sir_community');
-define('SIR_THEME_NAME', 'sir_community_name');
-
-add_theme_support( 'gnucommece' );
-
-global $sir_comm_global;
-$sir_comm_global = array();
-?>
